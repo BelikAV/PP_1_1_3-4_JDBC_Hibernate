820df899bbc69aede507895fafd1adf15d063c22
BelikAV <belikv199027@gmail.com>
2025-10-27 00:59:08 +0300

    решил задачу 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,131 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
     public UserDaoHibernateImpl() {
+        this.sessionFactory = Util.getSessionFactory();
 
     }
 
 
     @Override
     public void createUsersTable() {
+        String sql =
+                "CREATE TABLE IF NOT EXISTS users (" +
+                        "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                        "name VARCHAR(45), " +
+                        "last_name VARCHAR(100), " +
+                        "age TINYINT(3))";
 
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users";
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            tx.commit();
+
+        } catch (HibernateException e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+           session.save(new User(name, lastName, age));
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList= new ArrayList<>();
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            userList = session.createQuery("from User", User.class).getResultList();
+            tx.commit();
+        }catch (HibernateException e){
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        }
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            session.createQuery("delete from User").executeUpdate();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
 
@@ -8,7 +16,7 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/jdbc_hibernate";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "96321456Zxx";
-//    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static Connection connection;
 
 
@@ -17,11 +25,12 @@ public class Util {
 
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             System.out.println("Connected to database");
-        } catch (SQLException e ) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return connection;
     }
+
     public static void closeResources() {
         if (connection != null) {
             try {
@@ -30,5 +39,39 @@ public class Util {
                 throw new RuntimeException(e);
             }
         }
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
     }
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, DRIVER);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.HBM2DDL_AUTO, "none");
+                settings.put(Environment.SHOW_SQL, true);
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
 }
